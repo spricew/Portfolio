e5038b463cfec1a9e1dc951375ac6369e9c6f63e
Heyder Medina <medinaheyder@gmail.com>
2026-01-18 16:59:20 -0600

    chore: update ProjectProps to use new image assets for project covers and change imageSrc type to ImageMetadata

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,9 +1,14 @@
+import mayanlifeCover from "@/assets/projectCovers/mayanlife.webp"
+import spacexCover from "@/assets/projectCovers/spacex.png"
+import keidotCover from "@/assets/projectCovers/keidot.png"
+import portfolioCover from "@/assets/projectCovers/portfolio.png"
+
 export interface ProjectProps {
     previewLink?: string;
     githubLink?: string;
     projectTitle: string;
     projectDescription: string;
-    imageSrc: string;
+    imageSrc: ImageMetadata;
     imageAlt: string;
     techs: string[];
   }
@@ -13,7 +18,7 @@ export interface ProjectProps {
       previewLink: "https://mayanlifeyucatan.com/",
       projectTitle: "Mayan Life Inmobiliaria Web",
       projectDescription: "Mayan Life is a real estate website designed to showcase properties in a clear and easy-to-navigate way. It allows users to explore listings through filters, image galleries, and detailed information, offering a smooth experience for clients and visitors. Additionally, it includes an admin panel for managing properties and user roles.",
-      imageSrc: "images/mayanlife.png",
+      imageSrc: mayanlifeCover,
       imageAlt: "website preview",
       techs: ["NextJs", "TailwindCSS", "Express", "PostgreSQL"]
     }, 
@@ -22,7 +27,7 @@ export interface ProjectProps {
       previewLink: "https://spacex-dashboard-spricew.vercel.app",
       projectTitle: "SpaceX Dashboard",
       projectDescription: "SpaceX Dashboard is a web application that displays relevant information about SpaceX launches in a clear and organized way. It allows users to explore past, recent, and upcoming launches, showing details such as launch dates, mission status, rockets, and launchpads. The project focuses on a clean, user-friendly interface with reusable components and smooth navigation. It was built as a frontend project to practice API consumption, data handling, and interface design.",
-      imageSrc: "images/spacex.png",
+      imageSrc: spacexCover,
       imageAlt: "website preview",
       techs: ["NextJs", "TypeScript", "TailwindCSS"]
     },
@@ -30,7 +35,7 @@ export interface ProjectProps {
       githubLink: "https://github.com/spricew/Keidot-Project",
       projectTitle: "Keidot App",
       projectDescription: "Keidot is a mobile app that connects clients with workers quickly and safely. It allows users to request gardening services and displays nearby workers based on location. The process includes requesting a service, worker acceptance, and secure payment. The app also features a review system to ensure quality. Keidot aims to be a modern and accessible solution for those who need help in their garden and for individuals looking to earn extra income.",
-      imageSrc: "images/keidot.png",
+      imageSrc: keidotCover,
       imageAlt: "Keidot App",
       techs: ["Flutter", "Dart"]
     },
@@ -39,7 +44,7 @@ export interface ProjectProps {
       githubLink: "https://github.com/spricew/Portfolio",
       projectTitle: "Personal portfolio",
       projectDescription: "My personal portfolio is a website where I showcase who I am, what I do, and the projects I've built. I developed it using Astro and Tailwind to achieve a clean and easy-to-navigate design. It includes informative sections, all intended to present my work clearly. It's a space I will continue improving as I learn new things, and it represents my journey and growth as a web developer.",
-      imageSrc: "images/portfolio.png",
+      imageSrc: portfolioCover,
       imageAlt: "Portafolio personal",
       techs: ["AstroJs", "TailwindCSS"]
     }
